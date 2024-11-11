578df37fc8834fe847d50bbc73020a19166c31a8
Juan_Tobon <124205942+JuanTobon7@users.noreply.github.com>
2024-11-11 08:53:16 -0500

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,9 @@
 const { Pool } = require('pg');
 const db = new Pool({
-    user: 'postgres.basyflwabtgjovjndbum',
-    host: 'aws-0-us-west-1.pooler.supabase.com',
-    database: 'ecommerce',
-    password: 'j1122920156T1.',
+    user: procces.env.DATABASEUSER,
+    host: procces.env.DATABASEHOST,
+    database: procces.env.DATABASE,
+    password: process.env.DATABASEPASSWD,
     port: 5432,
 })
 
